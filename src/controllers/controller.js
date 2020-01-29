7af519b541464f5e7b93c1a720a1528a4a7340eb
JunMA <ma.jun2@husky.neu.edu>
2020-01-29 12:16:24 -0500

    upload whole work

--- src/controllers/controller.js
+++ src/controllers/controller.js
@@ -11,7 +11,7 @@ var auth = require('basic-auth')
  * @param {request} {HTTP request object}
  * @param {response} {HTTP response object}
  */
-exports.create = function (request, response) {
+exports.userCreate = function (request, response) {
     var email=request.body.email_address;
     //regular check for email address
     var re = /^(([^<>()\[\]\\.,;:\s@"]+(\.[^<>()\[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
@@ -63,7 +63,7 @@ exports.create = function (request, response) {
  * @param {request} {HTTP request object}
  * @param {response} {HTTP response object}
  */
-exports.get = function (request, response) {
+exports.userGet = function (request, response) {
     
     var credentials = auth(request)
     
@@ -115,7 +115,7 @@ exports.get = function (request, response) {
  * @param {request} {HTTP request object}
  * @param {response} {HTTP response object}
  */
-exports.update = function (request, response) {
+exports.userUpdate = function (request, response) {
     //seperate parameters
     var strongRegex = new RegExp("^(?=.*[a-z])(?=.*[A-Z])(?=.*[0-9])(?=.*[!@#\$%\^&\*])(?=.{8,})");
     if(!strongRegex.test(String(request.body.password)))
@@ -143,7 +143,7 @@ exports.update = function (request, response) {
                         response.json()
                     }
                     if(res){                        
-                        const sql=`UPDATE \`user\` SET \`first_name\`='${first_name}',\`last_name\`='${last_name}',\`password\`='${password}' WHERE \`ID\`=${data.rows[0].ID}`
+                        const sql=`UPDATE \`user\` SET \`first_name\`='${first_name}',\`last_name\`='${last_name}',\`password\`='${password}' WHERE \`ID\`='${data.rows[0].ID}'`
                         //access granted begin update
                         //console.log(sql)
                         query(sql).then(function(){
@@ -167,6 +167,388 @@ exports.update = function (request, response) {
     }
 };
 /**
+ * Returns a members object in JSON.
+ *
+ * @param {request} {HTTP request object}
+ * @param {response} {HTTP response object}
+ */
+exports.getBills = function (request, response) {
+    
+    var credentials = auth(request)
+    
+    if (!credentials) {
+        response.statusCode = 401
+        response.json();
+    } else {
+        //user existance check
+        query(`SELECT * FROM user WHERE email_address='${credentials.name}'`).then(function (data) {
+            if(data.rows[0]!=undefined)
+            {
+                //user exist
+                //console.log(data.rows[0])
+                bcrypt.compare(credentials.pass,data.rows[0].password,function(err, res) {
+                    //console.log(data.rows[0].password)
+                    if(err) {
+                        //server error...
+                        response.status(400)
+                        response.json()
+                        //console.log('Comparison error: ', err);
+                    }
+                    if(res){
+                        //login successfully, return data
+                        
+                        query(`SELECT * FROM Bill WHERE owner_id='${data.rows[0].ID}'`).then(function (data) {
+                            response.status(200)
+                            data.rows.forEach(element => {
+                                element.categories=csvToJsonList(element.categories)
+                                console.log(element.categories)
+                            });
+                            
+                            response.json(data.rows)
+                        })                      
+                        return
+                    }
+                    else
+                    {
+                        //password wrong
+                        response.status(401)
+                        response.json()
+                        return
+                    }                    
+                })                
+                return;
+            }
+            else{
+                response.status(401)
+                response.json()
+            }            
+        }).catch(renderErrorResponse(response));
+    }
+};
+/**
+ * Creates a new member 
+ * @param {request} {HTTP request object}
+ * @param {response} {HTTP response object}
+ */
+exports.createBill = function (request, response) {
+    
+    var credentials = auth(request)
+    
+    if(!(request.body.paymentStatus=="paid" || request.body.paymentStatus=="due" || request.body.paymentStatus=="no_payment_required" || request.body.paymentStatus=="past_due")){
+        
+        response.status(400)
+        response.json()
+        return
+    }
+
+    if (!credentials) {
+        response.statusCode = 401
+        response.json();
+    } else {
+        //user existance check
+        query(`SELECT * FROM user WHERE email_address='${credentials.name}'`).then(function (data) {
+            if(data.rows[0]!=undefined)
+            {
+                //user exist
+                //console.log(data.rows[0])
+                bcrypt.compare(credentials.pass,data.rows[0].password,function(err, res) {
+                    //console.log(data.rows[0].password)
+                    if(err) {
+                        //server error...
+                        response.status(400)
+                        response.json()
+                        //console.log('Comparison error: ', err);
+                    }
+                    if(res){
+                        //login successfully, return data
+                        const sql=`INSERT INTO \`Bill\`(\`id\`, \`owner_id\`, \`vendor\`, \`bill_date\`, \`due_date\`, \`amount_due\`, \`categories\`, \`paymentStatus\`) VALUES (UUID(),'${data.rows[0].ID}','${request.body.vendor}','${request.body.bill_date}','${request.body.due_date}','${request.body.amount_due}','${jsonListToCsv(request.body.categories)}',"${request.body.paymentStatus}")`
+                        console.log(sql)
+                        query(sql).then(function (data) {
+                            response.status(201)
+                            response.json()
+                            return
+                        }).catch(renderErrorResponse)
+                        // response.status(400)
+                        // response.json()                    
+                        return
+                    }
+                    else
+                    {
+                        //password wrong
+                        response.status(401)
+                        response.json()
+                        return
+                    }                    
+                })                
+                return;
+            }
+            else{
+                response.status(401)
+                response.json()
+            }            
+        }).catch(renderErrorResponse(response));
+    }
+};
+/**
+ * Returns a members object in JSON.
+ *
+ * @param {request} {HTTP request object}
+ * @param {response} {HTTP response object}
+ */
+exports.getBills = function (request, response) {
+    
+    var credentials = auth(request)
+    
+    if (!credentials) {
+        response.statusCode = 401
+        response.json();
+    } else {
+        //user existance check
+        query(`SELECT * FROM user WHERE email_address='${credentials.name}'`).then(function (data) {
+            if(data.rows[0]!=undefined)
+            {
+                //user exist
+                //console.log(data.rows[0])
+                bcrypt.compare(credentials.pass,data.rows[0].password,function(err, res) {
+                    //console.log(data.rows[0].password)
+                    if(err) {
+                        //server error...
+                        response.status(400)
+                        response.json()
+                        //console.log('Comparison error: ', err);
+                    }
+                    if(res){
+                        //login successfully, return data
+                        
+                        query(`SELECT * FROM Bill WHERE owner_id='${data.rows[0].ID}'`).then(function (data) {
+                            response.status(200)
+                            data.rows.forEach(element => {
+                                element.categories=csvToJsonList(element.categories)
+                                //console.log(element.categories)
+                            });
+                            
+                            response.json(data.rows)
+                        })                      
+                        return
+                    }
+                    else
+                    {
+                        //password wrong
+                        response.status(401)
+                        response.json()
+                        return
+                    }                    
+                })                
+                return;
+            }
+            else{
+                response.status(401)
+                response.json()
+            }            
+        }).catch(renderErrorResponse(response));
+    }
+};
+/**
+ * Returns a members object in JSON.
+ *
+ * @param {request} {HTTP request object}
+ * @param {response} {HTTP response object}
+ */
+exports.getBill = function (request, response) {
+    
+    var credentials = auth(request)
+    
+    if (!credentials) {
+        response.statusCode = 401
+        response.json();
+    } else {
+        //user existance check
+        query(`SELECT * FROM user WHERE email_address='${credentials.name}'`).then(function (data) {
+            if(data.rows[0]!=undefined)
+            {
+                //user exist
+                //console.log(data.rows[0])
+                bcrypt.compare(credentials.pass,data.rows[0].password,function(err, res) {
+                    //console.log(data.rows[0].password)
+                    if(err) {
+                        //server error...
+                        response.status(400)
+                        response.json()
+                        //console.log('Comparison error: ', err);
+                    }
+                    if(res){
+                        //login successfully, return data
+                        const sql=`SELECT * FROM Bill WHERE owner_id='${data.rows[0].ID}' AND id='${request.params.id}'`
+                        //console.log(sql)
+                        query(sql).then(function (data) {
+                            if(data.rows[0]==undefined){
+                                response.status(400);
+                                response.json();
+                                return;
+                            }
+                            response.status(200)
+                            data.rows.forEach(element => {
+                                element.categories=csvToJsonList(element.categories)
+                                //console.log(element.categories)
+                            });
+                            
+                            response.json(data.rows[0])
+                        })                      
+                        return
+                    }
+                    else
+                    {
+                        //password wrong
+                        response.status(401)
+                        response.json()
+                        return
+                    }                    
+                })                
+                return;
+            }
+            else{
+                response.status(401)
+                response.json()
+            }            
+        }).catch(renderErrorResponse(response));
+    }
+};
+/**
+ * Returns a members object in JSON.
+ *
+ * @param {request} {HTTP request object}
+ * @param {response} {HTTP response object}
+ */
+exports.putBill = function (request, response) {
+    
+    var credentials = auth(request)
+    if(!(request.body.paymentStatus=="paid" || request.body.paymentStatus=="due" || request.body.paymentStatus=="no_payment_required" || request.body.paymentStatus=="past_due")){
+        
+        response.status(400)
+        response.json()
+        return
+    }
+    if (!credentials) {
+        response.statusCode = 401
+        response.json();
+    } else {
+        //user existance check
+        query(`SELECT * FROM user WHERE email_address='${credentials.name}'`).then(function (data) {
+            if(data.rows[0]!=undefined)
+            {
+                //user exist
+                //console.log(data.rows[0])
+                bcrypt.compare(credentials.pass,data.rows[0].password,function(err, res) {
+                    //console.log(data.rows[0].password)
+                    if(err) {
+                        //server error...
+                        response.status(400)
+                        response.json()
+                        //console.log('Comparison error: ', err);
+                    }
+                    if(res){
+                        //login successfully, return data
+                        var owner_id=data.rows[0].ID
+                        var ticketid=request.params.id
+                        const sql=`SELECT * FROM Bill WHERE owner_id='${owner_id}' AND id='${ticketid}'`
+                        
+                        //console.log(sql)
+                        query(sql).then(function (data) {
+                            if(data.rows[0]==undefined){
+                                response.status(400);
+                                response.json();
+                                return;
+                            }
+                            const sql=`UPDATE \`Bill\` SET \`vendor\`='${request.body.vendor}',\`bill_date\`='${request.body.bill_date}',\`due_date\`='${request.body.due_date}',\`amount_due\`='${request.body.amount_due}',\`categories\`='${jsonListToCsv(request.body.categories)}',\`paymentStatus\`='${request.body.paymentStatus}' WHERE owner_id='${owner_id}' AND id='${ticketid}'`
+                            console.log(sql)
+                            query(sql).then(function (data) {
+                                response.status(204)                            
+                                response.json()
+                                return
+                            });
+                            
+                        })                      
+                        return
+                    }
+                    else
+                    {
+                        //password wrong
+                        response.status(401)
+                        response.json()
+                        return
+                    }                    
+                })                
+                return;
+            }
+            else{
+                response.status(401)
+                response.json()
+            }            
+        }).catch(renderErrorResponse(response));
+    }
+};
+/**
+ * Returns a members object in JSON.
+ *
+ * @param {request} {HTTP request object}
+ * @param {response} {HTTP response object}
+ */
+exports.deleteBill = function (request, response) {
+    
+    var credentials = auth(request)
+    if (!credentials) {
+        response.statusCode = 401
+        response.json();
+    } else {
+        //user existance check
+        query(`SELECT * FROM user WHERE email_address='${credentials.name}'`).then(function (data) {
+            if(data.rows[0]!=undefined)
+            {
+                //user exist
+                //console.log(data.rows[0])
+                bcrypt.compare(credentials.pass,data.rows[0].password,function(err, res) {
+                    //console.log(data.rows[0].password)
+                    if(err) {
+                        //server error...
+                        response.status(400)
+                        response.json()
+                        //console.log('Comparison error: ', err);
+                    }
+                    if(res){
+                        //login successfully, return data
+                        var owner_id=data.rows[0].ID
+                        var ticketid=request.params.id
+                        const sql=`DELETE FROM \`Bill\` WHERE owner_id='${owner_id}' AND id='${ticketid}'`
+                        
+                        //console.log(sql)
+                        query(sql).then(function (data) {
+                            
+                            response.status(204)                            
+                            response.json()
+                            return
+                            
+                            
+                        })                      
+                        return
+                    }
+                    else
+                    {
+                        //password wrong
+                        response.status(401)
+                        response.json()
+                        return
+                    }                    
+                })                
+                return;
+            }
+            else{
+                response.status(401)
+                response.json()
+            }            
+        }).catch(renderErrorResponse(response));
+    }
+};
+/**
  * Throws error if error object is present.
  *
  * @param {Response} response The response object
@@ -183,3 +565,18 @@ let renderErrorResponse = (response) => {
     }
     return errorCallback;
 };
+
+
+//small tools
+let jsonListToCsv=(json)=>{
+    var csv = '';
+    json.forEach(element => {
+        csv+=element+','
+    });
+    
+    return csv.substring(0,csv.length-1);
+}
+let csvToJsonList=(csv)=>{
+    var json=csv.split(",");  
+    return json; 
+}
