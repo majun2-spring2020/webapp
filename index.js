ad8905bfaa7cda2b24298fd33bdb5191cb9cc3df
JunMA1997 <ma.jun2@husky.neu.edu>
2020-02-19 23:20:19 -0500

    change test

--- index.js
+++ index.js
@@ -21,34 +21,34 @@ exec(`mkdir upload`)
 exec(`mysql -u ${default_user} -p${default_pass} < ${query_file}`,function(err,stdout,stderr){
     if(err) throw err
     
-    console.log("finish init")
-      
-    let express = require('express'),
-    app = express(),
-    port = process.env.PORT || 3000,
-    bodyParser = require('body-parser');
-
-
-    app.use(express.static(__dirname));//use dir
-    //Adding body parser for handling request and response objects.
-    app.use(bodyParser.urlencoded({
-        extended: true
-    }));
-    app.use(bodyParser.json());
-    //Enabling CORS
-    app.use(function (req, res, next) {
-        res.header("Access-Control-Allow-Origin", "*");
-        res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
-        next();
-    });
-
-    //Initialize app
-    let initApp = require('./src/app');
-    initApp(app);
-
-
-    console.log('server started on: ' + port);
-
-    module.exports = app.listen(port);
-
 })
+
+console.log("finish init")
+      
+let express = require('express'),
+app = express(),
+port = process.env.PORT || 3000,
+bodyParser = require('body-parser');
+
+
+app.use(express.static(__dirname));//use dir
+//Adding body parser for handling request and response objects.
+app.use(bodyParser.urlencoded({
+    extended: true
+}));
+app.use(bodyParser.json());
+//Enabling CORS
+app.use(function (req, res, next) {
+    res.header("Access-Control-Allow-Origin", "*");
+    res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+    next();
+});
+
+//Initialize app
+let initApp = require('./src/app');
+initApp(app);
+
+
+console.log('server started on: ' + port);
+
+module.exports = app.listen(port);
