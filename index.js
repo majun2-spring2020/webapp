f39cf9da2fa7dfd0d07250d99acbc5df9f68cbe1
JunMA1997 <ma.jun2@husky.neu.edu>
2020-02-23 18:06:15 -0500

    test rds

--- index.js
+++ index.js
@@ -10,30 +10,30 @@ mysqlconfig();
 
 
 
-// let express = require('express'),
-// app = express(),
-// port = process.env.PORT || 3000,
-// bodyParser = require('body-parser');
+let express = require('express'),
+app = express(),
+port = process.env.PORT || 3000,
+bodyParser = require('body-parser');
 
 
-// app.use(express.static(__dirname));//use dir
-// //Adding body parser for handling request and response objects.
-// app.use(bodyParser.urlencoded({
-//     extended: true
-// }));
-// app.use(bodyParser.json());
-// //Enabling CORS
-// app.use(function (req, res, next) {
-//     res.header("Access-Control-Allow-Origin", "*");
-//     res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
-//     next();
-// });
+app.use(express.static(__dirname));//use dir
+//Adding body parser for handling request and response objects.
+app.use(bodyParser.urlencoded({
+    extended: true
+}));
+app.use(bodyParser.json());
+//Enabling CORS
+app.use(function (req, res, next) {
+    res.header("Access-Control-Allow-Origin", "*");
+    res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+    next();
+});
 
-// //Initialize app
-// let initApp = require('./src/app');
-// initApp(app);
+//Initialize app
+let initApp = require('./src/app');
+initApp(app);
 
 
-// console.log('server started on: ' + port);
+console.log('server started on: ' + port);
 
-// module.exports = app.listen(port);
+module.exports = app.listen(port);
--- mysqlconfig.js
+++ mysqlconfig.js
@@ -14,15 +14,18 @@ module.exports=function(){
         con.query(`CREATE DATABASE IF NOT EXISTS \`${database}\`;`,function(err,result){
             con.query(`CREATE TABLE IF NOT EXISTS \`${database}\`.\`user\` ( \`ID\` VARCHAR(40) NOT NULL PRIMARY KEY, \`first_name\` TEXT NOT NULL , \`last_name\` TEXT NOT NULL , \`password\` TEXT NOT NULL , \`email_address\` VARCHAR(100) NOT NULL UNIQUE, \`account_created\` DATETIME NOT NULL DEFAULT CURRENT_TIMESTAMP, \`account_updated\` DATETIME NOT NULL DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP) ENGINE = InnoDB;`,function(err,result){
                 if(err) throw err
+                console.log(`finish user`)
             })
             con.query(`CREATE TABLE IF NOT EXISTS \`${database}\`.\`attachment\` (\`id\` VARCHAR(40) NOT NULL PRIMARY KEY , \`file_name\` TEXT NOT NULL , \`url\` TEXT NOT NULL , \`upload_date\` DATETIME NOT NULL DEFAULT CURRENT_TIMESTAMP , \`lastModifiedDate\` TEXT NOT NULL , \`SIZE\` INT NOT NULL DEFAULT 0 , \`MD5\` TEXT NOT NULL ) ENGINE = InnoDB;`,function(err,result){
                 if(err) throw err
+                console.log(`finish attachment`)
             })
             con.query(`CREATE TABLE IF NOT EXISTS \`${database}\`.\`Bill\` ( \`id\` VARCHAR(40) NOT NULL PRIMARY KEY, \`created_ts\` DATETIME NOT NULL DEFAULT CURRENT_TIMESTAMP, \`updated_ts\` DATETIME NOT NULL DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP, \`owner_id\` VARCHAR(40) NOT NULL , \`attachment_id\` VARCHAR(40), \`vendor\` TEXT NOT NULL , \`bill_date\` DATETIME NOT NULL , \`due_date\` DATETIME NOT NULL , \`amount_due\` DOUBLE NOT NULL , \`categories\` TEXT NOT NULL , \`paymentStatus\` ENUM('paid','due','past_due','no_payment_required') , FOREIGN KEY(attachment_id) REFERENCES \`attachment\`(ID) ON DELETE SET NULL, FOREIGN KEY(owner_id) REFERENCES \`user\`(ID) ON DELETE CASCADE) ENGINE = InnoDB`,function(err,result){
                 if(err) throw err
+                console.log(`finish bill`)
             })
         })
-        console.log("finish")
+        console.log(`finish database`)
     })
     
 }
