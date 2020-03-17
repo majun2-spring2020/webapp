db6e98250cb3e82e512e13947fcde8e2fb6ee627
JunMA <ma.jun2@husky.neu.edu>
2020-03-16 21:00:27 -0400

    change path

--- scripts/npm-start.sh
+++ scripts/npm-start.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 cd /webapp
 source /etc/profile
-sudo /opt/aws/amazon-cloudwatch-agent/bin/amazon-cloudwatch-agent-ctl -a fetch-config -m ec2 -c file:~/webapp/cloudwatch_config.json -s
+sudo /opt/aws/amazon-cloudwatch-agent/bin/amazon-cloudwatch-agent-ctl -a fetch-config -m ec2 -c file:/webapp/cloudwatch_config.json -s
 nohup npm start >~/output 2>&1 &
