2cfab7a34558c4069009e90773a70847debded81
JunMA1997 <ma.jun2@husky.neu.edu>
2020-03-17 13:54:02 -0400

    test codedeploy

--- scripts/npm-start.sh
+++ scripts/npm-start.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 cd /webapp
-# source /etc/profile
+source /etc/profile
 sudo /opt/aws/amazon-cloudwatch-agent/bin/amazon-cloudwatch-agent-ctl -a fetch-config -m ec2 -c file:/webapp/cloudwatch_config.json -s
-sudo nohup npm start > /opt/application.log 2>&1 &
+nohup npm start > ~/application.log 2>&1 &
