83b9296447e17fc2836fa456dc6713f98d53f94a
JunMA1997 <ma.jun2@husky.neu.edu>
2020-03-17 13:41:18 -0400

    try use ubuntu user

--- scripts/npm-start.sh
+++ scripts/npm-start.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 cd /webapp
-source /etc/profile
+# source /etc/profile
 sudo /opt/aws/amazon-cloudwatch-agent/bin/amazon-cloudwatch-agent-ctl -a fetch-config -m ec2 -c file:/webapp/cloudwatch_config.json -s
-nohup npm start >~/output 2>&1 &
+nohup npm start > /opt/application.log 2>&1 &
