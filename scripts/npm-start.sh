90847428912d446dd4fdfd9d535e915624d88c88
JunMA1997 <ma.jun2@husky.neu.edu>
2020-04-01 18:12:42 -0400

    test all

--- scripts/npm-start.sh
+++ scripts/npm-start.sh
@@ -3,3 +3,4 @@ cd /webapp
 source /etc/profile
 sudo /opt/aws/amazon-cloudwatch-agent/bin/amazon-cloudwatch-agent-ctl -a fetch-config -m ec2 -c file:/webapp/cloudwatch_config.json -s
 nohup npm start > ~/application.log 2>&1 &
+nohup node SQSChildProcess.js > ~/application.log 2>&1 &
